58241aaf8ca61d980c8f4fe95b749966241187ec
Thomas Spaulding <tspauldi@assurerx.local>
2018-04-26 15:50:15 -0400

    fixing link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <router-view></router-view>
         <div>test</div>
-        <a href="../react/index.html">Go To React Project</a>
+        <a href="../react-app/index.html">Go To React Project</a>
     </div>
 </template>
 
